1c9057bade24cb19a83b4bfb0b66833f9aa55f68
Mandy <mandy.reinhardt@posteo.de>
2018-04-15 12:15:19 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let filterName = function () {
+    let input, filter, ul, li, a, i;
+    input = document.getElementById('name');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ulNames");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
